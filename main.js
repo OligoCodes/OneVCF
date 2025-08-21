63e8d5df1bdd1b5ebe4c54290bcd12dad60b8e3e
Joseph Kwabena Osei Bonsu <josephoseibonsu742@gmail.com>
2025-08-21 23:12:12 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  alert("After submiting contact,  please follow the channels below to claim file")
+}, 3000)
+
+const submitBtn = document.querySelector(".return")
+
+submitBtn.addEventListener('click', (event) => {
+ event.preventDefault(); //stop button from reloading page
+ 
+ const name = document.getElementById("name");
+ const number = document.getElementById("phoneNumber");
+ 
+ const options = {
+   method: "POST",
+   headers: {
+     "Content-Type":"application/json"},
+   body: JSON.stringify({name, number})
+ }
+ 
+ const url= 'https://onevcf-backend.onrender.com/save';
+
+fetch(url, options)
+.then(res => res.json())
+.then(data => {
+  console.log(data)
+})
+.catch(err => {
+  console.error("Error: ", err)
+})
+  
+  
+  
+  
+});
